4166c0279892b4a2df08e93e10b7c16c0de70669
nlc <NineLifeCat@yandex.ru>
2020-12-09 22:40:31 +0300

    refactor SelenideMainPageTests, TestBase

--- src/test/java/tests/SelenideMainPageTests.java
+++ src/test/java/tests/SelenideMainPageTests.java
@@ -1,13 +1,16 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
-public class SelenideMainPageTests {
+public class SelenideMainPageTests extends TestBase{
+
+
     @Test
     public void openMainSelenidePage(){
       step("Открываем главную страницу", () ->{
