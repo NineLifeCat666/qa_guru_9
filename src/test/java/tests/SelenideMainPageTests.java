f0d3c1e5ecc16294da48b148856dc2cd60351096
nlc <NineLifeCat@yandex.ru>
2020-12-09 21:57:49 +0300

    Added AttachmentsHelper, TestBase and changed build.gradle

---
+++ src/test/java/tests/SelenideMainPageTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class SelenideMainPageTests {
+    @Test
+    public void openMainSelenidePage(){
+      step("Открываем главную страницу", () ->{
+          open("https://selenide.org");
+      });
+      step("Проверяем что на главной странице присутствует текст What is Selenide?", ()->{
+          $(".main").shouldHave(Condition.text("What is Selenide?"));
+      });
+    }
+}
+
