b676bb0696f6b6a191b9f66d9470b0e56db74582
nlc <NineLifeCat@yandex.ru>
2020-12-13 20:53:38 +0300

    Cosmetic refactor SelenideMainPageTests

--- src/test/java/tests/SelenideMainPageTests.java
+++ src/test/java/tests/SelenideMainPageTests.java
@@ -1,22 +1,20 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
-public class SelenideMainPageTests extends TestBase{
-
+public class SelenideMainPageTests extends TestBase {
 
     @Test
-    public void openMainSelenidePage(){
-      step("Открываем главную страницу", () ->{
+    public void openMainSelenidePage() {
+      step("Открываем главную страницу", () -> {
           open("https://selenide.org");
       });
-      step("Проверяем что на главной странице присутствует текст What is Selenide?", ()->{
+      step("Проверяем что на главной странице присутствует текст What is Selenide?", () -> {
           $(".main").shouldHave(Condition.text("What is Selenide?"));
       });
     }
